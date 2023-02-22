cea82f020b207b9642840ac553dc8d5ab89be5fe
ttyg567 <ttyg567naver.com>
2023-02-22 17:55:01 +0900

    20230222

---
+++ day02/src/day02/P55.java
@@ -0,0 +1,15 @@
+package day02;
+
+public class P55 {
+
+	public static void main(String[] args) {
+		int a = 2000000000;
+		int b = 2000000000;
+		double A = a;
+		double B = b;
+		double result = A + B;
+		System.out.println(result);
+
+	}
+
+}
